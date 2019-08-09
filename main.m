a9b67b9a60b65c25c80caa4d9c70593ed0df3868
Roman Tihai <46790893+Rtihai@users.noreply.github.com>
2019-08-08 22:56:10 -0700

    Create main.m

---
+++ main.m
@@ -0,0 +1,38 @@
+#import <Foundation/Foundation.h>
+#import "InputHandler.h"
+#import "ScoreKeeper.h"
+#import "QuestionsController.h"
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        
+        ScoreKeeper *scorekeeper = [[ScoreKeeper alloc] init];
+        
+        while (YES)
+        {
+            QuestionsController *questions = [[QuestionsController alloc]init];
+            Question *question = [questions question];
+            if ([[[question userInput] uppercaseString] isEqualToString:@"QUIT"])
+            {
+                break;
+            } else
+            {
+                if ([question isAnswer])
+                {
+                    [scorekeeper setRightCount:[scorekeeper rightCount]+1];
+                } else
+                {
+                    [scorekeeper setWrongCount:[scorekeeper wrongCount]+1];
+                }
+                
+                [scorekeeper printScore];
+                [scorekeeper setLastTime:[question answerTime]];
+                [scorekeeper printAverageTime];
+            }
+        }
+        
+    }
+    return 0;
+}
